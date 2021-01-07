5853306e98413ca64f331ac63fa96635f0d5c30c
Malvika Grover <malvikagrover22@gmail.com>
2021-01-07 15:57:29 +0530

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,57 @@
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        int myFirstNumber = 5;
+//        int mySecondNumber = 6;
+//        int myThirdNumber = 12;
+//        int myTotal = myFirstNumber+mySecondNumber+ myThirdNumber;
+//        System.out.println(myTotal);
+//        int myLastOne = myTotal-1000;
+//        System.out.println(myLastOne);
+//
+//        int myMinValue = Integer.MIN_VALUE;
+//        int myMaxValue = Integer.MAX_VALUE;
+//        System.out.println("Minimum Integer Value: " + myMinValue + "\n" + "Maximum Integer Value: " + myMaxValue);
+//        System.out.println("Overflow " + (myMaxValue+1));
+//        System.out.println("Underflow: " + (myMinValue-1));
+//
+//        long myLongValue = 10L;
+//        System.out.println(myLongValue);
+        //we use L in the end of long declaration because if we don't do that, the computer takes it as
+        //an integer
+
+
+
+
+//        byte newByteValue = 10;
+//        short newShortValue = 20;
+//        int newIntValue = 30;
+//        long newLongValue = 50000+10*(newByteValue+newShortValue+newIntValue);
+//        System.out.println(newLongValue);
+//
+//        short shortValue = (short) (50000+10*(newByteValue+newShortValue+newIntValue));
+
+//        int a = 5 / 3;
+//        float b = 5f / 3f;
+//        double c = 5d / 3d;
+//        System.out.println("Integer: " + a);
+//        System.out.println("Float: " + b);
+//        System.out.println("Double: " + c);
+
+//        double pound = 10d;
+//        double kilogram = 0.45359237d * pound;
+//        System.out.println("Total kilogram is: " + kilogram);
+
+        char first = 'D';
+        char second = '\u00a9';
+        System.out.println(first);
+        System.out.println(second);
+
+
+
+
+
+
+
+    }
+}
