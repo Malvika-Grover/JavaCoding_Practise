7e871ec1a73cfaf09193088e69165394a61a4796
Malvika Grover <malvikagrover22@gmail.com>
2021-01-10 11:31:36 +0530

    Methods

--- src/Hello.java
+++ src/Hello.java
@@ -46,21 +46,54 @@ public class Hello {
 //        char second = '\u00a9';
 //        System.out.println(first);
 //        System.out.println(second);
+//
+//        int abc = 100;
+//        if (abc == 10){
+//            System.out.println("You've scored = "+ abc + " YAAYYYY !!!!");
+//        }
+//        else
+//            System.out.println("You've scored = "+ abc + " You're failing, buckle up");
 
-        int abc = 100;
-        if (abc == 10){
-            System.out.println("You've scored = "+ abc + " YAAYYYY !!!!");
+//        boolean is_active = true;
+//        if(is_active = true){
+//            System.out.println("Hello.... Welcome Home");
+//        }
+
+//        int a = 11;
+//        boolean b = (a == 10) ? true : false ;
+//        if (b)
+//            System.out.println("b won");
+//        else
+//            System.out.println("a won");
+
+/*
+        double firstVariable = 20.00d;
+        double secondVariable = 80.00d;
+        double caclulation1 = (firstVariable+secondVariable)*100.00d;
+        double calculation2 = caclulation1 % 40.00d;
+        boolean values = (calculation2 == 0 ) ? true : false;
+        System.out.println(values);
+        if (values == !true){
+            System.out.println("Got some reminder");
+        }*/
+
+/*        boolean gameOver = true;
+        int score = 5000;
+        int level = 5;
+        int bonus = 800;*/
+        Calculations(true, 499, 5, 100);
+
+
+    }
+
+    public static int Calculations(boolean gameOver, int score, int level, int bonus ) {
+        int finalScore = 0;
+        if (score >= 500 && level >= 5) {
+            finalScore = score + (level * bonus);
+            System.out.println("You have done a great job!! Your score is :" + finalScore);
+        } else {
+            System.out.println("Try again next time, Your score is : " + finalScore);
         }
-        else
-            System.out.println("You've scored = "+ abc + " You're failing, buckle up");
-
-
-
-
-
-
-
-
-
+        return finalScore;
     }
 }
